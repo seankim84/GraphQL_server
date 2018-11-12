18de16558a789b706f1f99c415baff5194c697ad
SeanKim <sean.kim84@gmail.com>
2018-11-12 15:57:53 +0700

    Request the RESTAPI

--- db/db.js
+++ db/db.js
@@ -1,85 +1,16 @@
-export let movies = [
-    {
-        id: 0,
-        name: "sean",
-        age: 29,
-        gender: "Male",
-        score: 0.1
-    },
-    {
-        id: 1,
-        name: "Rebekah",
-        age: 27,
-        gender: "Female",
-        score: 0.3
-    },
-    {
-        id: 2,
-        name: "Nam",
-        age: 19,
-        gender: "Male",
-        score: 0.4
-    },
-    {
-        id: 3,
-        name: "Eun",
-        age: 29,
-        gender: "Female",
-        score: 0.2
-    },
-    {
-        id: 4,
-        name: "Jin",
-        age: 26,
-        gender: "Tae",
-        score: 0.6
-    },
-    {
-        id: 5,
-        name: "Gi",
-        age: 30,
-        gender: "Male",
-        score: 0.9
-    },
-    {
-        id: 6,
-        name: "Tae",
-        age: 31,
-        gender: "Male",
-        score: 1.2
-    },
-    {
-        id: 7,
-        name: "Ho",
-        age: 66,
-        gender: "Male",
-        score: 2.5
+import fetch from 'node-fetch';
+
+const API_URL = `https://yts.am/api/v2/list_movies.json?`;
+
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if(limit > 0){
+        REQUEST_URL += `limit=${limit}`
     }
-]
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovie = movies.filter(movie => movie.id === id);
-    return filteredMovie[0];
-}   
-
-export const deleteMovie = id => {
-    const cleanMovies = movies.filter(movie => movie.id != id)
-    if(movies.length > cleanMovies.length){ // 현재 movie의 배열의 길이가 clenaMovie 배열의 길이보다 길다면
-        movies = cleanMovies; // movie를 cleanMovies로 지정한다.
-        return true;
-    } else {
-        return false;
+    if(rating > 0){ 
+        REQUEST_URL += `&minimum_rating=${rating}`;
     }
-};
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${ movies.length + 1 }`, // 자동으로 이 길이에 + 1을 한 값이 Id가 된다.
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
+    return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json=>json.data.movies)
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,16 +1,12 @@
 //Resolver(해결)이란 query를 해결하는것이다. query는 database에게 문제와 같은것이다.
 //그래서 우리는 이 query를 어떤 방식으로든 resolve(해결) 해야 한다.(query를 위한 해결!)
-import { getMovies, getById, addMovie, deleteMovie } from '../db/db';
+import { getMovies } from '../db/db';
 
 const resolvers = {
     Query : {
-        movies: () => getMovies(),
-        movie: ( _, { id }) => getById(id)
-    },
-    Mutation: {
-        addMovie: ( _, { name, score } ) => addMovie(name, score),
-        deleteMovie: ( _, { id }) => deleteMovie(id)
+        movies: (_, {limit, rating}) => getMovies(limit, rating) // '_'는 그냥 variable 이름이다.
     }
+    
 };
 
 export default resolvers;
